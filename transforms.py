8c95d5997ffa1811215a0d0c19c1f5edec0c341d
Seungjin Lee <lsg0385@naver.com>
2020-08-30 15:48:25 +0000

    [add, Da] add efficient net for vad code

---
+++ transforms.py
@@ -0,0 +1,217 @@
+import tensorflow as tf
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+EPSILON = 1e-8
+LOG_EPSILON = tf.math.log(EPSILON)
+
+
+""" FEATURE INDEPENDENT AUGMENTATIONS """
+def mask(specs, axis, max_mask_size=None, n_mask=1):
+    def make_shape(size):
+        # returns (1, ..., size, ..., 1)
+        shape = [1] * len(specs.shape)
+        shape[axis] = size
+        return tuple(shape)
+
+    total = specs.shape[axis]
+    mask = tf.ones(make_shape(total), dtype=specs.dtype)
+    if max_mask_size is None:
+        max_mask_size = total
+
+    for i in range(n_mask):
+        size = tf.random.uniform([], maxval=max_mask_size, dtype=tf.int32)
+        offset = tf.random.uniform([], maxval=total-size, dtype=tf.int32)
+
+        mask *= tf.concat(
+            (tf.ones(shape=make_shape(offset), dtype=mask.dtype),
+             tf.zeros(shape=make_shape(size), dtype=mask.dtype),
+             tf.ones(shape=make_shape(total-size-offset), dtype=mask.dtype)),
+            axis=axis)
+
+    return specs * mask
+
+
+def random_shift(specs, axis=0, width=16):
+    new_specs = tf.pad(specs, [[0]*2 if i != axis else [width]*2
+                               for i in range(len(specs.shape))])
+    new_specs = tf.image.random_crop(new_specs, specs.shape)
+    return new_specs
+
+
+def cutmix(xs, ys):
+    '''
+    xs : [batch, height, width, chan] np.ndarray
+    ys : [batch, n_classes] np.ndarray
+    '''
+    xs = tf.cast(xs, tf.float32)
+    ys = tf.cast(ys, tf.float32)
+    batch, height, width, chan = xs.shape
+
+    # select lambda
+    sqrt_lmbda = tf.math.sqrt(tf.random.uniform([], maxval=1.))
+
+    # set size of window and recalculate lambda
+    size_h = tf.cast(height * sqrt_lmbda, dtype=tf.int32)
+    size_w = tf.cast(width * sqrt_lmbda, dtype=tf.int32)
+
+    # select window
+    offset_h = tf.random.uniform([], maxval=height-size_h, dtype=tf.int32)
+    offset_w = tf.random.uniform([], maxval=width-size_w, dtype=tf.int32)
+
+    windows = tf.ones([size_h, size_w], dtype=tf.float32)
+    windows = tf.pad(windows,
+                     [[offset_h, height-offset_h-size_h],
+                      [offset_w, width-offset_w-size_w]])
+    windows = tf.reshape(windows, (1, height, width, 1))
+
+    # shuffle
+    indices = tf.math.reduce_mean(tf.ones_like(ys), axis=-1)
+    indices = tf.cast(tf.math.cumsum(indices, exclusive=True), dtype='int32')
+    indices = tf.random.shuffle(indices)
+
+    # mix
+    xs = xs * (1-windows) + tf.gather(xs, indices, axis=0) * windows
+    mean = tf.cast((size_h * size_w) / (height * width), dtype=tf.float32)
+    ys = ys * (1-mean) + tf.gather(ys, indices, axis=0) * mean
+
+    return xs, ys
+
+
+def interclass(mix_func):
+    def _interclass(xs, ys):
+        '''
+        xs : [batch, height, width, chan] np.ndarray
+        ys : [batch, n_classes] np.ndarray
+        '''
+        xs_, ys_ = [], []
+        cls = tf.argmax(ys, axis=-1)
+
+        for i in range(ys.shape[-1]):
+            select = tf.squeeze(tf.where(cls == i), axis=-1)
+            xs_temp = tf.gather(xs, select, axis=0)
+            ys_temp = tf.gather(ys, select, axis=0)
+            xs_temp, ys_temp = mix_func(xs_temp, ys_temp)
+            xs_.append(xs_temp)
+            ys_.append(ys_temp)
+
+        return tf.concat(xs_, axis=0), tf.concat(ys_, axis=0)
+    return _inter
+
+
+def interbinary(mix_func):
+    ''' 
+    modified interclass mixup
+    returns mixup(audio with voice) + mixup(non-voice)
+    '''
+    def _interbinary(xs, ys):
+        '''
+        xs : [batch, height, width, chan] np.ndarray
+        ys : [batch, n_classes] np.ndarray
+        '''
+        xs_, ys_ = [], []
+        cls = tf.argmax(ys, axis=-1)
+
+        for i in range(2):
+            if i == 0:
+                select = tf.where(cls != ys.shape[-1]-1)
+            else:
+                select = tf.where(cls == ys.shape[-1]-1)
+            select = tf.squeeze(select, axis=-1)
+            xs_temp = tf.gather(xs, select, axis=0)
+            ys_temp = tf.gather(ys, select, axis=0)
+            xs_temp, ys_temp = mix_func(xs_temp, ys_temp)
+            xs_.append(xs_temp)
+            ys_.append(ys_temp)
+
+        return tf.concat(xs_, axis=0), tf.concat(ys_, axis=0)
+    return _interbinary
+
+
+""" MAGNITUDE-PHASE SPECTROGRAM """
+def random_magphase_flip(spec, label):
+    flip = tf.cast(tf.random.uniform([]) > 0.5, spec.dtype)
+    n_chan = spec.shape[-1] // 2
+    chans = tf.reshape(tf.range(n_chan*2), (2, n_chan))
+    chans = tf.reshape(tf.reverse(chans, axis=[-1]), (-1,))
+    spec = flip*spec + (1-flip)*tf.gather(spec, chans, axis=-1)
+
+    flip = tf.cast(flip, label.dtype)
+    label = flip*label \
+            + (1-flip)*tf.concat(
+                [tf.reverse(label[..., :-1], axis=(-1,)), label[..., -1:]],
+                axis=-1)
+
+    return spec, label
+
+
+def magphase_mixup(alpha=2.):
+    beta = tf.compat.v1.distributions.Beta(alpha, alpha)
+
+    def _mixup(specs, labels):
+        # preprocessing
+        specs = tf.cast(specs, dtype=tf.float32)
+        labels = tf.cast(labels, dtype=tf.float32)
+
+        indices = tf.reduce_mean(
+            tf.ones_like(labels, dtype=tf.int32),
+            axis=range(1, len(labels.shape)))
+        indices = tf.cumsum(indices, exclusive=True)
+        indices = tf.random.shuffle(indices)
+
+        # assume mag, phase...
+        n_chan = specs.shape[-1] // 2
+        mag, phase = specs[..., :n_chan], specs[..., n_chan:]
+
+        real = mag * tf.cos(phase)
+        img = mag * tf.sin(phase)
+
+        l = beta.sample()
+
+        real = l*real + (1-l)*tf.gather(real, indices, axis=0)
+        img = l*img + (1-l)*tf.gather(img, indices, axis=0)
+        
+        mag = tf.math.sqrt(real**2 + img**2)
+        phase = tf.math.atan2(img, real)
+
+        specs = tf.concat([mag, phase], axis=-1)
+        labels = tf.cast(labels, tf.float32)
+        labels = l*labels + (1-l)*tf.gather(labels, indices, axis=0)
+        
+        return specs, labels
+
+    return _mixup
+
+
+def log_magphase(specs, labels=None):
+    n_chan = specs.shape[-1] // 2
+    specs = tf.concat(
+            [tf.math.log(specs[..., :n_chan]+EPSILON), specs[..., n_chan:]],
+            axis=-1)
+    if labels is not None:
+        return specs, labels
+    return specs
+
+
+def minmax_norm_magphase(specs, labels=None):
+    n_chan = specs.shape[-1] // 2
+    mag = specs[..., :n_chan]
+    phase = specs[..., n_chan:]
+    axis = tuple(range(1, len(specs.shape)))
+
+    mag_max = tf.math.reduce_max(mag, axis=axis, keepdims=True)
+    mag_min = tf.math.reduce_min(mag, axis=axis, keepdims=True)
+    phase_max = tf.math.reduce_max(phase, axis=axis, keepdims=True)
+    phase_min = tf.math.reduce_min(phase, axis=axis, keepdims=True)
+
+    specs = tf.concat(
+        [(mag-mag_min)/(mag_max-mag_min+EPSILON),
+         (phase-phase_min)/(phase_max-phase_min+EPSILON)],
+        axis=-1)
+
+    if labels is not None:
+        return specs, labels
+    return specs
+
+
+""" MEL-SPECTROGRAM """
+
---
+++ utils_test.py
@@ -0,0 +1,107 @@
+import numpy as np
+import unittest
+from utils import *
+
+
+class UtilsTest(unittest.TestCase):
+    def test_degree_to_class(self):
+        degrees = [-1, 0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
+        target = [10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        self.assertEqual(target, 
+                         degree_to_class(degrees, 20, 0, 180, False).tolist())
+        self.assertEqual(np.eye(11)[target].tolist(), 
+                         degree_to_class(degrees, 20, 0, 180, True).tolist())
+
+    def test_class_to_degree(self):
+        classes = [10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual([-1, 0, 20, 40, 60, 80, 100, 120, 140, 160, 180],
+                         class_to_degree(classes, 20, 0, 180, -1).tolist())
+
+    def test_seq_to_windows(self):
+        seq = np.array([1, 2, 3, 4, 5])
+        window = np.array([-3, -1, 0, 1, 3])
+
+        target = np.array([[0, 0, 1, 2, 4],
+                           [0, 1, 2, 3, 5],
+                           [0, 2, 3, 4, 0],
+                           [1, 3, 4, 5, 0],
+                           [2, 4, 5, 0, 0]])
+        self.assertEqual(target.tolist(), 
+                         seq_to_windows(seq, window).tolist())
+        self.assertEqual(target[::2].tolist(),
+                         seq_to_windows(seq, window, 2).tolist())
+
+    def test_windows_to_seq(self):
+        windows = np.array([[0, 0, 1, 2, 4],
+                            [0, 1, 2, 3, 5],
+                            [0, 2, 3, 4, 0],
+                            [1, 3, 4, 5, 0],
+                            [2, 4, 5, 0, 0]])
+        window = np.array([-3, -1, 0, 1, 3])
+
+        target = np.array([1, 2, 3, 4, 5])
+        self.assertTrue(
+            np.allclose(target, windows_to_seq(windows, window)))
+        self.assertTrue(
+            np.allclose(target, windows_to_seq(windows[::2], window, skip=2)))
+
+    def test_window_generator(self):
+        np.random.seed(0)
+        specs = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        labels = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+        target = zip(
+            [[1, 2, 3], [3, 4, 5], [4, 5, 6], [6, 7, 8]],
+            [2, 4, 5, 7])
+        gen = window_generator(specs, labels, 3, infinite=False)
+        output = [(s, l) for s, l in gen()]
+
+        for (t_s, t_l), (s, l) in zip(target, output):
+            self.assertEqual(t_s, s.tolist())
+            self.assertEqual(t_l, l.tolist())
+
+        # infinite test
+        win_size = 3
+        gen = window_generator(specs, labels, win_size, infinite=True)
+        for i, (s, l) in enumerate(gen()):
+            if i > len(specs) * 2:
+                break
+            self.assertEqual(win_size, len(s))
+            # self.assertEqual(1, len(l))
+
+    def test_apply_kernel_regularizer(self):
+        n_samples, in_shape, out_shape = 128, 4, 4
+        x = np.random.randn(n_samples, in_shape)
+        y = np.random.randint(out_shape, size=n_samples)
+
+        # model without regularizer
+        tf.random.set_seed(0)
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Input(shape=(in_shape,)))
+        model.add(tf.keras.layers.Dense(out_shape, activation='softmax'))
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+
+        model.fit(x, y, verbose=False)
+        base_weights = model.weights[:]
+
+        # model with regularizer
+        tf.random.set_seed(0)
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Input(shape=(in_shape,)))
+        model.add(tf.keras.layers.Dense(out_shape, activation='softmax'))
+
+        model = apply_kernel_regularizer(model, tf.keras.regularizers.l2(0.1))
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+
+        model.fit(x, y, verbose=False)
+        new_weights = model.weights[:]
+
+        for b, n in zip(base_weights, new_weights):
+            self.assertNotEqual(b.numpy().tolist(), n.numpy().tolist())
+        
+
+if __name__ == '__main__':
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    unittest.main()
